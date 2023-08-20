92b104828ac2c4a8f9042d71962591484ad39a55
Gabe Young <Gabe_young@outlook.com>
2023-08-21 09:16:19 +0930

    Add project files.

---
+++ WebApplication3/Views/Home/Contact Us.cshtml.cs
@@ -0,0 +1,12 @@
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace WebApplication3.Views.Home
+{
+    public class Contact_UsModel : PageModel
+    {
+        public void OnGet()
+        {
+        }
+    }
+}
---
+++ WebApplication3/Views/Home/Veterinary Staff.cshtml.cs
@@ -0,0 +1,12 @@
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace WebApplication3.Views.Home
+{
+    public class Virtual_Valleys_Veterinary_StaffModel : PageModel
+    {
+        public void OnGet()
+        {
+        }
+    }
+}
